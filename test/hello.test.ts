cc449966d1834f915a4cde02769d4ff0419c1ac1
Juan Jesús Padrón Hernández <jpadron@opencanarias.es>
2020-12-01 20:40:47 +0000

    Test and parseIndividualNumber function refactor

--- test/hello.test.ts
+++ test/hello.test.ts
@@ -1,8 +1,6 @@
-import { parseNumber } from "../src/hello_world";
+import { parseIndividualNumber } from "../src/hello_world";
 
-describe("Bank OCR", ()=>{
-    it("parses a number from card format to string", () => {
-        const numberOne = "   \n" +
+const numberOne = "   \n" +
                           "  |\n" +
                           "  |\n"
     
@@ -41,16 +39,17 @@ describe("Bank OCR", ()=>{
         const numberZero =  " _ \n" +
                             "| |\n" +
                             "|_|\n" 
-
-        expect(parseNumber(numberOne)).toBe("1");
-        expect(parseNumber(numberTwo)).toBe("2");
-        expect(parseNumber(numberThree)).toBe("3");
-        expect(parseNumber(numberFour)).toBe("4");
-        expect(parseNumber(numberFive)).toBe("5");
-        expect(parseNumber(numberSix)).toBe("6");
-        expect(parseNumber(numberSeven)).toBe("7");
-        expect(parseNumber(numberEight)).toBe("8");
-        expect(parseNumber(numberNine)).toBe("9");
-        expect(parseNumber(numberZero)).toBe("0");
+describe("Bank OCR", ()=>{
+    it("parses a number from card format to string", () => {
+        expect(parseIndividualNumber(numberOne)).toBe("1");
+        expect(parseIndividualNumber(numberTwo)).toBe("2");
+        expect(parseIndividualNumber(numberThree)).toBe("3");
+        expect(parseIndividualNumber(numberFour)).toBe("4");
+        expect(parseIndividualNumber(numberFive)).toBe("5");
+        expect(parseIndividualNumber(numberSix)).toBe("6");
+        expect(parseIndividualNumber(numberSeven)).toBe("7");
+        expect(parseIndividualNumber(numberEight)).toBe("8");
+        expect(parseIndividualNumber(numberNine)).toBe("9");
+        expect(parseIndividualNumber(numberZero)).toBe("0");
     })
 })
